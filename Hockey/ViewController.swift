be47333653f874d506f5e1e9e97d604bfbf5e2c2
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-04-12 14:21:11 -0500

    Added views

--- Hockey/ViewController.swift
+++ Hockey/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// must be >= 1.0
+    var snapX:CGFloat = 40.0
+    
+    /// must be >= 1.0
+    var snapY:CGFloat = 1.0
+    
+    /// how far to move before dragging
+    var threshold:CGFloat = 0.0
+    
+    /// the guy we're dragging
+    var selectedView:UIView?
+    
+    /// drag in the Y direction?
+    var shouldDragY = true
+    
+    /// drag in the X direction?
+    var shouldDragX = true
+    
+
+    
+    
+    
+    var dynamicAnimator = UIDynamicAnimator()
+    var collisionBehavior = UICollisionBehavior()
+
+    @IBOutlet weak var playerOneView: UIView!
+    @IBOutlet weak var playerTwoView: UIView!
+    @IBOutlet weak var puckView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /******Turn UIViews into Circles******/
+        playerOneView.layer.cornerRadius = playerOneView.frame.size.width/2
+        playerOneView.clipsToBounds = true
+        
+        playerTwoView.layer.cornerRadius = playerTwoView.frame.size.width/2
+        playerTwoView.clipsToBounds = true
+        
+        puckView.layer.cornerRadius = puckView.frame.size.width/2
+        puckView.clipsToBounds = true
+        /************************************/
+
+
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /**********Set Orientation**********/
+
+    override func shouldAutorotate() -> Bool {
+        return true
     }
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return [UIInterfaceOrientationMask.LandscapeLeft,UIInterfaceOrientationMask.LandscapeRight]
+    }
+    /************************************/
 
-
+    
+    
+    
+    
 }
 
