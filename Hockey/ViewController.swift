a65dc826d53da91fac0698e8b33c7fc1ef67091e
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-04-12 14:38:31 -0500

    Newest

--- Hockey/ViewController.swift
+++ Hockey/ViewController.swift
@@ -9,28 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    /// must be >= 1.0
-    var snapX:CGFloat = 40.0
-    
-    /// must be >= 1.0
-    var snapY:CGFloat = 1.0
-    
-    /// how far to move before dragging
-    var threshold:CGFloat = 0.0
-    
-    /// the guy we're dragging
-    var selectedView:UIView?
-    
-    /// drag in the Y direction?
-    var shouldDragY = true
-    
-    /// drag in the X direction?
-    var shouldDragX = true
-    
-
-    
-    
     
     var dynamicAnimator = UIDynamicAnimator()
     var collisionBehavior = UICollisionBehavior()
@@ -42,35 +20,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /******Turn UIViews into Circles******/
-        playerOneView.layer.cornerRadius = playerOneView.frame.size.width/2
-        playerOneView.clipsToBounds = true
-        
-        playerTwoView.layer.cornerRadius = playerTwoView.frame.size.width/2
-        playerTwoView.clipsToBounds = true
-        
-        puckView.layer.cornerRadius = puckView.frame.size.width/2
-        puckView.clipsToBounds = true
-        /************************************/
-
-
-        
+                
     }
 
-    
-    /**********Set Orientation**********/
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        return [UIInterfaceOrientationMask.LandscapeLeft,UIInterfaceOrientationMask.LandscapeRight]
-    }
-    /************************************/
-
-    
-    
-    
-    
 }
 
