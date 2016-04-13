d8f9b08b028d167f81ca2058f54c37a9484e47cb
student3 <student3@jh-mbair-158-09.d214.org>
2016-04-13 14:19:20 -0500

    hehehehhehehe

--- Hockey/ViewController.swift
+++ Hockey/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
+    var array:[UIView] = []
     
     var dynamicAnimator = UIDynamicAnimator()
     var collisionBehavior = UICollisionBehavior()
@@ -19,18 +20,38 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupViews()
+       
+    }
+    func setupViews(){
         puckView.backgroundColor = UIColor.blueColor()
         view.addSubview(puckView)
-        
-       // addDynamicBehavior(puckView)
     }
-
     @IBAction func hockeyStickPanGestureRecognizer(sender: UIPanGestureRecognizer) {
         let panGesture = sender.locationInView(view)
         hockeyStickView.center = CGPointMake(panGesture.x, hockeyStickView.center.y)
         dynamicAnimator.updateItemUsingCurrentState(hockeyStickView)
     }
+    
+    func addDynamicBehavior(array: [UIView]){
+        let dynamicItemBehavior = UIDynamicItemBehavior(items: array)
+        dynamicItemBehavior.density = 1.0
+        dynamicItemBehavior.elasticity = 1.0
+        dynamicItemBehavior.friction = 0.0
+        dynamicItemBehavior.resistance = 0.0
+        dynamicAnimator.addBehavior(dynamicItemBehavior)
+        
+        let pushBehavior = UIPushBehavior(items: array, mode: .Instantaneous)
+        pushBehavior.magnitude = 1.0
+        pushBehavior.pushDirection = CGVectorMake(0.5, 0.5)
+        dynamicAnimator.addBehavior(pushBehavior)
+        
+        
+        let collisionBehavior = UICollisionBehavior(items: array)
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        collisionBehavior.collisionMode = .Everything
+        collisionBehavior.collisionDelegate = self
+        dynamicAnimator.addBehavior(collisionBehavior)
 
-
+    }
 }
