bef45da7e65a8d0cffdb45148fe717f816c997c8
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-04-13 14:41:51 -0500

    new

--- Hockey/ViewController.swift
+++ Hockey/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
     var array:[UIView] = []
+    var hockeyStickArray:[UIView] = []
+    var bothArray:[UIView] = []
+
+
     
     var dynamicAnimator = UIDynamicAnimator()
     var collisionBehavior = UICollisionBehavior()
@@ -21,8 +25,17 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         array.append(puckView)
+        bothArray.append(puckView)
+        puckView.clipsToBounds = true
         view.addSubview(puckView)
 
+        hockeyStickArray.append(hockeyStickView)
+        hockeyStickView.clipsToBounds = true
+
+        bothArray.append(hockeyStickView)
+
+        view.addSubview(hockeyStickView)
+        
         
         addDynamicBehavior()
 
@@ -41,23 +54,32 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     func addDynamicBehavior()
     {
         
+        let stickDynamicItemBehavior = UIDynamicItemBehavior(items: hockeyStickArray)
+        stickDynamicItemBehavior.density = 100000.0
+        stickDynamicItemBehavior.elasticity = 1.0
+        stickDynamicItemBehavior.friction = 0.0
+        stickDynamicItemBehavior.resistance = 0.0
+        dynamicAnimator.addBehavior(stickDynamicItemBehavior)
+        
         let dynamicItemBehavior = UIDynamicItemBehavior(items: array)
         dynamicItemBehavior.density = 1.0
         dynamicItemBehavior.elasticity = 1.0
         dynamicItemBehavior.friction = 0.0
         dynamicItemBehavior.resistance = 0.0
         dynamicAnimator.addBehavior(dynamicItemBehavior)
+
+        
         
         let pushBehavior = UIPushBehavior(items: array, mode: .Instantaneous)
         pushBehavior.magnitude = 1.0
         pushBehavior.pushDirection = CGVectorMake(0.5, 0.5)
         dynamicAnimator.addBehavior(pushBehavior)
         
-        
-        let collisionBehavior = UICollisionBehavior(items: array)
+        let collisionBehavior = UICollisionBehavior(items: bothArray)
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
         collisionBehavior.collisionMode = .Everything
         collisionBehavior.collisionDelegate = self
+
         dynamicAnimator.addBehavior(collisionBehavior)
 
     }
